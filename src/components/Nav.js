e91eacd9ba72e200db084e2412968ba62a374cba
Ansh Chauhan <ansh.chauhan269@gmail.com>
2023-01-08 01:18:32 +0530

    frontend try 1

---
+++ src/components/Nav.js
@@ -0,0 +1,29 @@
+
+const Nav = ({ authToken, minimal, setShowModal, showModal, setIsSignUp }) => {
+  const handleClick = () => {
+    setShowModal(true);
+    setIsSignUp(false);
+  };
+
+  return (
+    <nav>
+      <div className="logo-container">
+        <img
+          className="logo"
+          src = "../images/tindin.jpeg"
+          alt="logo"
+        />
+      </div>
+      {!authToken && !minimal && (
+        <button
+          className="nav-button"
+          onClick={handleClick}
+          disabled={showModal}
+        >
+          Log in
+        </button>
+      )}
+    </nav>
+  );
+};
+export default Nav;
